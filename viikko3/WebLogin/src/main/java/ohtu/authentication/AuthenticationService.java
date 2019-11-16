74eb22373e7a63b9bed1fd4169e4000bc43719e7
Saara Koskipaa <saara.koskipaa@iki.fi>
2019-11-16 13:21:32 +0200

    Viikko 3, valmis

--- viikko3/WebLogin/src/main/java/ohtu/authentication/AuthenticationService.java
+++ viikko3/WebLogin/src/main/java/ohtu/authentication/AuthenticationService.java
@@ -33,12 +33,38 @@ public class AuthenticationService {
         if (username.length()<3 ) {
             status.addError("username should have at least 3 characters");
         }
+        
+        if (!username.matches("[a-z]+")) {
+            status.addError("username should consist only of letters a-z");
+        }
+        
+        if (password.length() < 8) {
+            status.addError("password should have at least 8 characters");
+        }
+        
+        if (checkIfPasswordContainsOnlyLetters(password)) {
+            status.addError("password can not contain only letters");
+        }
+        
+        if (!password.equals(passwordConfirmation)) {
+            status.addError("password and password confirmation do not match");
+        }
 
         if (status.isOk()) {
             userDao.add(new User(username, password));
         }
         
         return status;
+    }
+    
+     private boolean checkIfPasswordContainsOnlyLetters(String password) {
+        for (int i = 0; i < password.length(); i++) {
+            if (Character.isLetter(password.charAt(i)) == false) {
+                return false;
+            }
+        }
+        
+        return true;
     }
 
 }
